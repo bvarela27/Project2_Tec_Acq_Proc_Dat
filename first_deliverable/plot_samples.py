4a89732ae76877ede6a4740547d472ab15b666e9
Steven <stevenrojascubero@gmail.com>
2022-04-30 15:57:39 -0600

    Added error

--- first_deliverable/plot_samples.py
+++ first_deliverable/plot_samples.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 data1 = np.loadtxt('samples.txt')
 data2 = np.loadtxt('decoder_output.txt')
 
+err = np.sum(np.absolute(data1-data2))/(data1.size*(2**16)/100)
+
 plt.plot(data1)
 plt.plot(data2)
+plt.figtext(0.7, 0.8, "Error: "+ "%.3f" % err +"%")
 plt.show()
